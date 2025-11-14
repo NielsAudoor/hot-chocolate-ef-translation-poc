5ebd202edb5b44e09a9a9f1506e8db10c2d58149
Niels Audoor <29575824+NielsAudoor@users.noreply.github.com>
2025-11-14 21:26:17 +0100

    Adds some default data seeding

--- DateTimeReproduction.Api/Program.cs
+++ DateTimeReproduction.Api/Program.cs
@@ -19,6 +19,38 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
+
+    if (!db.Events.Any())
+    {
+        db.Events.AddRange(
+            new Event
+            {
+                Name = "Conference Keynote",
+                Timestamp = new DateTimeOffset(2025, 11, 14, 9, 0, 0, TimeSpan.FromHours(1))
+            },
+            new Event
+            {
+                Name = "Workshop Session",
+                Timestamp = new DateTimeOffset(2025, 11, 14, 14, 30, 0, TimeSpan.FromHours(1))
+            },
+            new Event
+            {
+                Name = "Networking Event",
+                Timestamp = new DateTimeOffset(2025, 11, 15, 18, 0, 0, TimeSpan.FromHours(1))
+            },
+            new Event
+            {
+                Name = "Product Launch",
+                Timestamp = new DateTimeOffset(2025, 11, 16, 10, 0, 0, TimeSpan.FromHours(1))
+            },
+            new Event
+            {
+                Name = "Team Meeting",
+                Timestamp = new DateTimeOffset(2025, 11, 17, 13, 0, 0, TimeSpan.FromHours(1))
+            }
+        );
+        db.SaveChanges();
+    }
 }
 
 app.MapGraphQL();
